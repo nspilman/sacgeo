4fcf068db6bfe8b382db8683df9cb5edbca50ebe
Nate Spilman <natespilman@Nates-MBP.hsd1.nj.comcast.net>
2019-05-11 12:39:16 -0400

    updating links to open in new tab

--- src/components/socialMedia.vue
+++ src/components/socialMedia.vue
@@ -1,17 +1,17 @@
 <template>
 <div id='wrapper'>
     <div id ='socialMedia'>
-        <a href = "https://www.instagram.com/sac_geo/?hl=en">
+        <a href = "https://www.instagram.com/sac_geo/?hl=en" target="_blank">
             <div class = 'icon'>
                 <i class="fab fa-instagram"></i>
             </div>
         </a>
-        <a href = "https://www.youtube.com/channel/UCEfvR9o9e5gAt3fPI7_O4xg/">
+        <a href = "https://www.youtube.com/channel/UCEfvR9o9e5gAt3fPI7_O4xg/" target="_blank">
             <div class = 'icon'>
                 <i class="fab fa-youtube"></i>
             </div>
         </a>
-        <a href = "https://open.spotify.com/user/21oifm5tgutbg3sad2axb3cua/playlist/7jW9BhOQUurP100QrtuZVP?si=_UCa9vr_QwWXnS4--Af2EA">
+        <a href = "https://open.spotify.com/user/21oifm5tgutbg3sad2axb3cua/playlist/7jW9BhOQUurP100QrtuZVP?si=_UCa9vr_QwWXnS4--Af2EA" target="_blank">
             <div class = 'icon'>
                 <i class="fab fa-spotify"></i>
             </div>

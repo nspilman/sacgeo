2617ad9c3c298e60bca73c37fad098e71ff9a531
Nate Spilman <natespilman@Nates-MBP.hsd1.nj.comcast.net>
2019-05-11 12:35:38 -0400

    adding links

--- src/components/socialMedia.vue
+++ src/components/socialMedia.vue
@@ -1,17 +1,23 @@
 <template>
 <div id='wrapper'>
     <div id ='socialMedia'>
-        <div class = 'icon'>
+        <a href = "https://www.instagram.com/sac_geo/?hl=en">
+            <div class = 'icon'>
                 <i class="fab fa-instagram"></i>
             </div>
-                <div class = 'icon'>
-                    <i class="fab fa-youtube"></i>
-                </div>
-                        <div class = 'icon'>
-                            <i class="fab fa-spotify"></i>
-                        </div>
-        </div>
-        </div>
+        </a>
+        <a href = "https://www.youtube.com/channel/UCEfvR9o9e5gAt3fPI7_O4xg/">
+            <div class = 'icon'>
+                <i class="fab fa-youtube"></i>
+            </div>
+        </a>
+        <a href = "https://open.spotify.com/user/21oifm5tgutbg3sad2axb3cua/playlist/7jW9BhOQUurP100QrtuZVP?si=_UCa9vr_QwWXnS4--Af2EA">
+            <div class = 'icon'>
+                <i class="fab fa-spotify"></i>
+            </div>
+        </a>
+    </div>
+</div>
 </template>
 
 <script>
